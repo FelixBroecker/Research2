6279a9a3497f1256ef853bd1e277f9d5722f17bb
FelixBroecker <felixbroecker90@gmail.com>
2024-04-24 15:00:16 +0200

    add calculation of projection matrix and diagonalization using lapack

--- coding/symmetricDavidson.f90
+++ coding/symmetricDavidson.f90
@@ -1,8 +1,10 @@
 program davidson
   implicit none
-  
-  real, allocatable :: matA(:,:), matV(:,:), matW(:,:), diagonalA(:)
-  integer :: i, j, it, ndimA, ndimV, maxiter, idxMaxVal(2)
+ 
+  intrinsic :: selected_real_kind
+  integer, parameter :: wp = selected_real_kind(15)
+  real(wp), allocatable :: matA(:,:), matV(:,:), matW(:,:), matP(:,:),  diagonalA(:), eigenvals(:), work(:)
+  integer :: i, j, it, ndimA, ndimV, maxiter, idxMaxVal(2), lwork, info
   
   ndimA = 5
   ndimV = 20
@@ -32,11 +34,25 @@ program davidson
   write(*,*) 'Initital Vector'
   call printMatrix(matV)
 
+!get projection of A
+  allocate(matW(ndimA, ndimV))
+  allocate(matP(ndimV, ndimV))
   do it = 1, 1
-    matW = matmul(matA, matV)
+    call dgemm('n', 'n', ndimA, ndimV, ndimA, 1.0d0, matA, ndimA, matV, ndimA, 0.0d0, matW, ndimA)
     write(*,*) 'Matrixproduct A V :'
-    call printMatrix(matW) 
+    call printMatrix(matW)
+
+    call dgemm('t', 'n', ndimV, ndimV, ndimA, 1.0d0, matW, ndimA, matV, ndimA, 0.0d0, matP, ndimV)
+    write(*,*) 'Matrixproduct (A V)T V'
+    call printMatrix(matP)
   end do
+
+!diagonalize 
+  allocate(eigenvals(ndimV))
+  lwork = max(1, 3 * ndimV - 1)
+  allocate(work(lwork))
+  call dsyev('V', 'U', ndimV, matP, ndimV, eigenvals, work, lwork, info)
+  write(*,*) 'Eigenvalues:', eigenvals
   
 !output
   write(*,*) 'Input Matrix A:'
@@ -47,7 +63,7 @@ program davidson
 contains
   subroutine printMatrix(mat) 
       integer :: i
-      real, allocatable :: mat(:,:)
+      real(wp), allocatable :: mat(:,:)
       do i = 1, size(mat(:,i))
         print *, mat(i, :)
       end do
