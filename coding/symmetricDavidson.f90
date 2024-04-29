53b0228b062b3e18e8834f2dcc265b3c441c606b
FelixBroecker <felixbroecker90@gmail.com>
2024-04-29 16:14:21 +0200

    Proposal for code to calculate Residuals

--- coding/symmetricDavidson.f90
+++ coding/symmetricDavidson.f90
@@ -4,10 +4,9 @@ program davidson
   intrinsic              :: selected_real_kind
   integer,  parameter    :: wp = selected_real_kind(15)
   real(wp)               :: lw(1)
-  real(wp), allocatable  :: matA(:,:), matV(:,:), matW(:,:), matP(:,:),  diagonalA(:), eigenvals(:), work(:) 
+  real(wp), allocatable  :: matA(:,:), matV(:,:), matW(:,:), matP(:,:),  diagonalA(:), eigenvals(:), eigenvecs(:,:), work(:)
+  real(wp), allocatable  :: ritzVector(:,:), identity(:,:), temp(:,:), ritzVectorTemp(:), residual(:)
   integer                :: i, j, it, ndimA, ndimV, maxiter, idxMaxVal(2), lwork, info
-  real(wp), allocatable  :: ritzVector(:,:)
-  real(wp), allocatable  :: ritzVector2(:,:)
 
   ndimA = 5
   ndimV = 6
@@ -33,7 +32,7 @@ program davidson
 
 !get initial vector
   allocate(matV( ndimA, ndimV ))
-
+  !search for colum with maximum value and use that column for initial vector
   idxMaxVal = maxloc(matA)
   matV(idxMaxVal(1), 1) = 1
   write(*,*) 'Initital Vector'
@@ -42,6 +41,7 @@ program davidson
 !get projection of A
   allocate(matW(ndimA, ndimV))
   allocate(matP(ndimV, ndimV))
+
   do it = 1, 1
     call dgemm('n', 'n', ndimA, ndimV, ndimA, 1.0d0, matA, ndimA, matV, ndimA, 0.0d0, matW, ndimA)
     write(*,*) 'Matrixproduct A V :'
@@ -50,36 +50,76 @@ program davidson
     call dgemm('t', 'n', ndimV, ndimV, ndimA, 1.0d0, matW, ndimA, matV, ndimA, 0.0d0, matP, ndimV)
     write(*,*) 'Matrixproduct (A V)T V'
     call printMatrix(matP)
+
+  !diagonalize and obtain eigenvalues and eigenvectors 
+    allocate(eigenvals(ndimV))
+    allocate(eigenvecs(ndimV, ndimV))
+
+    !store matP in eigenvecs because dsyev writes eigenvecs in input matrix
+    eigenvecs = matP
+
+    call dsyev('V', 'U', ndimV, eigenvecs, ndimV, eigenvals, lw, -1, info)
+    lwork = int(lw(1))
+    allocate(work(lwork))
+    call dsyev('V', 'U', ndimV, eigenvecs, ndimV, eigenvals, work, lwork, info)
+
+    write(*,*) 'Eigenvalues:', eigenvals
+    write(*,*) 'Matrix P'
+    call printMatrix(matP)
+    write(6,*) 'eigenvecs'
+    call printMatrix(eigenvecs)
+      
+  !get ritz vector with to matrices
+    write(*,*) 'Matrix V'
+    call printMatrix(matV)
+    allocate(ritzVector(ndimA,ndimV))
+    call dgemm('n', 'n', ndimA, ndimV, ndimV, 1.0d0, matV, ndimA, eigenvecs, ndimV, 0.0d0, ritzVector, ndimA)
+    write(*,*) 'Ritzvector'
+    call printMatrix(ritzVector)
+  
+  !get residual
+    !get identity matrix and put eigenvalues on diagonal and substract from matA
+    allocate(identity(ndimA, ndimA))
+    allocate(temp(ndimA, ndimA))
+
+    !print *, maxloc(eigenvals)
+    ! dot(eigenvals,  ritzvector)
+    !call dgemv('n', ndimA, ndimV, 1.0d0, ritzVector, ndimA, eigenvals, 1, 0.0d0, temp, 1)
+    !call dgemm('n', 'n', ndimA, ndimV, ndimV, -1.0d0, matW, ndimA, eigenvals, ndimV, 1.0d0, temp, ndimA)
+    !print *, 'temp mat'
+    !call printMatrix(temp)
+
+    identity(1:ndimA, 1:ndimA) = 0.0d0
+    forall(i = 1:ndimA) identity(i, i) = 1.0d0
+  
+    do i = 0, it - 1
+      !compute ritzvector for index i
+        allocate(ritzVectorTemp(ndimA))
+        allocate(residual(ndimA))
+        call dgemv('n', ndimA, ndimV, 1.0d0, matV, ndimA, eigenvecs(:, size( eigenvecs(1,:) ) - i), 1, 0.0d0, ritzVectorTemp, 1)
+        print *, 'ritz Vector Temp'
+        print *, ritzVectorTemp
+      !compute A - eigenval of index i * Identity
+        temp = matA - eigenvals(size(eigenvals) - i) * identity
+        print *, 'temp mat'
+        call printMatrix(temp)
+      !dot Product of both
+        call dgemv('n', ndimA, ndimA, 1.0d0, temp, ndimA, ritzVectorTemp, 1, 0.0d0, residual, 1)
+        print *, 'Vector r_i', residual
+
+
+    !call printMatrix(identity) 
+    enddo
+    
   end do
 
-!diagonalize 
-  allocate(eigenvals(ndimV))
-  call dsyev('V', 'U', ndimV, matP, ndimV, eigenvals, lw, -1, info)
-  lwork = int(lw(1))
-  allocate(work(lwork))
-  !lwork = max(1, 3 * ndimV - 1)
-  call dsyev('V', 'U', ndimV, matP, ndimV, eigenvals, work, lwork, info)
-  write(*,*) 'Eigenvalues:', eigenvals
-  write(*,*) 'Matrix P'
-  call printMatrix(matP)
-  
-  
-!get ritz vector
-  write(*,*) 'Matrix V'
-  call printMatrix(matV)
-  write(6,*) 'before'
-  allocate(ritzVector2(ndimA,ndimV))
-!  allocate(ritzVector(ndimA,ndimV))
-  !call dgemv('n', ndimA, ndimV, 1.0d0, matV, ndimA, eigenvals, 1, 0.0d0, ritzVector, 1)
-  !write(*,*) 'Ritzvector'
-  !call printMatrix(ritzVector)
-  write(6,*) 'matrix', ritzVector 
 
   contains
     subroutine printMatrix(mat) 
-        integer :: i
+        integer :: i, length
         real(wp), allocatable :: mat(:,:)
-        do i = 1, size(mat(:,i))
+        length = size(mat(:,i))
+        do i = 1, length
           print *, mat(i, :)
         end do
     end subroutine printMatrix
